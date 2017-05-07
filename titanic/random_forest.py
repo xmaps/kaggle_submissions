595bc100ec11ea1ba3445fe357ceb3e3a00ebfaf
Mário Santos <mariosantos@outlook.com>
2017-05-07 18:41:05 +0100

    select only the best features for random forest

--- titanic/random_forest.py
+++ titanic/random_forest.py
@@ -11,10 +11,12 @@
 # to split on. By averaging the predictions of all of the trees,
 # we get a stronger overall prediction and minimize overfitting.
 
-
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import cross_validation
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.cross_validation import KFold
 
 from feature_preparation import prepare_features
@@ -26,7 +28,21 @@ titanic = pd.read_csv("train.csv")
 # prepare all the necessary features
 titanic = prepare_features(titanic)
 
-predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked",
+              "FamilySize", "Title", "FamilyId", "NameLength"]
+
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+
+# Get the raw p-values for each feature, and transform them from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# Plot the scores
+# Do you see how "Pclass", "Sex", "Title", and "Fare" are the best features?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
 
 # Initialize our algorithm with the default paramters
 # n_estimators is the number of trees we want to make
@@ -39,8 +55,10 @@ predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 # different subsets of the data, having more trees will greatly increase accuracy (up to a point).
 # We can also tweak the min_samples_split and min_samples_leaf variables to reduce overfitting.
 
-alg = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=4, min_samples_leaf=2)
+# Pick only the four best features
+predictors = ["Pclass", "Sex", "Fare", "Title"]
 
+alg = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=8, min_samples_leaf=4)
 kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
 
 # Compute the accuracy score for all the cross-validation folds; this is much simpler than what we did before
